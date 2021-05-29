3655c957d32519447998551df26004361485a5a7
prasetyobella17 <danu.17bella@gmail.com>
2021-05-29 16:35:38 +0700

    Monifikasi Component Hero

--- src/components/HeroHome.vue
+++ src/components/HeroHome.vue
@@ -41,26 +41,6 @@
             </div>
           </div>
         </div>
-
-        <!-- Gambar3 -->
-        <div
-          class="single-hero-items set-bg"
-          style="background-image: url('/img/hero-3.jpg')"
-        >
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5">
-                <span>Bag,kids</span>
-                <h1>Black friday</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore
-                </p>
-                <a href="#" class="primary-btn">Shop Now</a>
-              </div>
-            </div>
-          </div>
-        </div>
       </carousel>
     </section>
     <!-- Hero Section End -->
