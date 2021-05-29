e0ea15b771aa3588aab4c45402fc8c8b38cc5379
prasetyobella17 <danu.17bella@gmail.com>
2021-05-29 16:18:53 +0700

    Menambahka Caraousel Hero

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <header-home></header-home>
+    <header-home />
+    <hero-home />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
 import HeaderHome from "../components/HeaderHome.vue";
+
+import HeroHome from "../components/HeroHome.vue";
 
 export default {
   name: "Home",
   components: {
     HeaderHome,
+    HeroHome,
   },
 };
 </script>
