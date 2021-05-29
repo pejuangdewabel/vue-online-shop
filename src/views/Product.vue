166b18c28c77ec58580a59cb2b8ce8ae6fecb034
prasetyobella17 <danu.17bella@gmail.com>
2021-05-29 20:46:31 +0700

    Add Halaman Product

---
+++ src/views/Product.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="product">
+    <header-home />
+    <p>
+      Halaman Product
+    </p>
+    <footer-home />
+  </div>
+</template>
+<script>
+import FooterHome from "../components/FooterHome.vue";
+import HeaderHome from "../components/HeaderHome.vue";
+export default {
+  name: "Product",
+  components: {
+    HeaderHome,
+    FooterHome,
+  },
+};
+</script>
