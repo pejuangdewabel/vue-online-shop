fdb652717fe57c0bc6a0377748175ac328b2bbc4
prasetyobella17 <danu.17bella@gmail.com>
2021-05-29 20:05:45 +0700

    Add Component Owl Product

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,22 @@
   <div class="home">
     <header-home />
     <hero-home />
+    <product-owl />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HeaderHome from "../components/HeaderHome.vue";
-
 import HeroHome from "../components/HeroHome.vue";
+import ProductOwl from "../components/ProductOwl.vue";
 
 export default {
   name: "Home",
   components: {
     HeaderHome,
     HeroHome,
+    ProductOwl,
   },
 };
 </script>
